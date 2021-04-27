59e1fe247bec78796d60c7fa56b6f41255d4ccda
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2021-04-27 00:35:33 -0400

    Update to the latest
    Also remove JCenter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,23 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.32"
-    id("org.jetbrains.compose") version "0.4.0-build182"
+    id("org.jetbrains.compose") version "0.4.0-build185"
 }
 
 group = "org.pushing-pixels.aurora.demo"
 version = "1.0.0"
 
 repositories {
-    jcenter()
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
 }
 
 dependencies {
-    implementation("org.pushing-pixels:aurora-skin:0.0.24-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-icon-icon:0.0.24-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-component:0.0.24-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-window:0.0.24-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-skin:0.0.27-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-icon-icon:0.0.27-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-component:0.0.27-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-window:0.0.27-SNAPSHOT")
     implementation(compose.desktop.currentOs)
     implementation("org.pushing-pixels:radiance-substance:4.0-SNAPSHOT")
 }
