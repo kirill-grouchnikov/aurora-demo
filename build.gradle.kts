f5a9b6587cf0f947883cb634d684ab0873cf1b2e
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2021-11-21 21:04:25 -0500

    Update to the latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0-beta5"
+    id("org.jetbrains.compose") version "1.0.0-beta6-dev464"
 }
 
 group = "org.pushing-pixels.aurora.demo"
@@ -14,13 +14,12 @@ repositories {
     mavenCentral()
     google()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
 }
 
 dependencies {
-    implementation("org.pushing-pixels:aurora-theming:0.0.59-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-component:0.0.59-SNAPSHOT")
-    implementation("org.pushing-pixels:aurora-window:0.0.59-SNAPSHOT")
+    implementation("org.pushing-pixels:aurora-theming:1.0.0-beta2")
+    implementation("org.pushing-pixels:aurora-component:1.0.0-beta2")
+    implementation("org.pushing-pixels:aurora-window:1.0.0-beta2")
     implementation(compose.desktop.currentOs)
     implementation("org.pushing-pixels:radiance-theming:5.0.0")
 }
