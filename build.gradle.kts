cd61a872d629b10896f9286ce488848a4e3e1506
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2022-02-24 18:59:57 -0500

    Upgrade to Aurora 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0"
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.0"
 }
 
 group = "org.pushing-pixels.aurora.demo"
@@ -16,9 +16,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.pushing-pixels:aurora-theming:1.0.1")
-    implementation("org.pushing-pixels:aurora-component:1.0.1")
-    implementation("org.pushing-pixels:aurora-window:1.0.1")
+    implementation("org.pushing-pixels:aurora-theming:1.1.0")
+    implementation("org.pushing-pixels:aurora-component:1.1.0")
+    implementation("org.pushing-pixels:aurora-window:1.1.0")
     implementation(compose.desktop.currentOs)
     implementation("org.pushing-pixels:radiance-theming:5.0.0")
 }
