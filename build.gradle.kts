9d35e11598bcbdde70089c72703b1eb2ebf309e1
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2025-09-29 19:30:09 -0400

    Upgrade to the latest
    Kotlin -> 2.1.20
    Compose -> 1.8.0
    Aurora -> 2.0 snapshot
    Radiance -> 8.5 snapshot
    Version checker -> 0.52.0
    Gradle -> 8.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,41 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("org.jetbrains.compose") version "1.3.0"
+    kotlin("multiplatform") version "2.1.20"
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 group = "org.pushing-pixels.aurora.demo"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
+    }
+
+    dependencies {
+        classpath(libs.compose.desktop)
+        classpath(libs.kotlin.gradlePlugin)
+        classpath(libs.aurora.theming)
+        classpath(libs.aurora.component)
+        classpath(libs.aurora.window)
+        classpath(libs.radiance.theming)
+    }
 }
 
-dependencies {
-    implementation(compose.desktop.currentOs)
-    implementation(libs.aurora.theming)
-    implementation(libs.aurora.component)
-    implementation(libs.aurora.window)
-    implementation(libs.radiance.theming)
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
 }
 
 configurations {
@@ -33,6 +47,24 @@ configurations {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
+}
+
+kotlin {
+    jvm("desktop")
+    sourceSets {
+        named("desktopMain") {
+            dependencies {
+                api(compose.runtime)
+                api(compose.foundation)
+                api(compose.desktop.common)
+                api(compose.desktop.currentOs)
+                api(libs.radiance.theming)
+                api(libs.aurora.component)
+                api(libs.aurora.theming)
+                api(libs.aurora.window)
+            }
+        }
+    }
 }
 
 compose.desktop {
